4a6a4e6e23bd9c6ae1b695090f183de0e200943f
Vijay Singh Rajput <vijay.rajput@gmail.com>
2016-04-14 22:12:36 +0100

    Update models.js

--- bootcamphtml5/fioricheckjobs/webapp/model/models.js
+++ bootcamphtml5/fioricheckjobs/webapp/model/models.js
@@ -1,42 +1,32 @@
 sap.ui.define([
-		"sap/ui/model/json/JSONModel",
-		"sap/ui/Device"
-	], function (JSONModel, Device) {
-		"use strict";
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], function(JSONModel, Device) {
+	"use strict";
 
-		return {
+	return {
 
-			createDeviceModel : function () {
-				var oModel = new JSONModel(Device);
-				oModel.setDefaultBindingMode("OneWay");
-				return oModel;
-			},
-
-			createFLPModel : function () {
-				var fnGetUser = jQuery.sap.getObject("sap.ushell.Container.getUser"),
-					bIsShareInJamActive = fnGetUser ? fnGetUser().isJamActive() : false,
-					oModel = new JSONModel({
-						isShareInJamActive: bIsShareInJamActive
-					});
-				oModel.setDefaultBindingMode("OneWay");
-				return oModel;
-			}
-// ***************************************************************************************************
+		createDeviceModel: function() {
+			var oModel = new JSONModel(Device);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		}
+		// ***************************************************************************************************
 		// Start of Custom code 
 		// User Information Model
-// ***************************************************************************************************
-			,
-			
-			createUserModel : function(){
-				var oModel = new JSONModel("/destinations/BootCampDestination/JobEnrollmentDemo/GetUserAttributes");
-				oModel.setDefaultBindingMode("OneWay");
-				return oModel;
-			}
-// ***************************************************************************************************
+		// ***************************************************************************************************
+		,
+
+		createUserModel: function() {
+		
+			var oModel = new JSONModel(window.oRootPath + "/destinations/BootCampDestination/JobEnrollmentDemo/GetUserAttributes");
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		}
+		// ***************************************************************************************************
 		// End of Custom code 
-// ***************************************************************************************************			
+		// ***************************************************************************************************
 
-		};
+	};
 
-	}
-);
+});

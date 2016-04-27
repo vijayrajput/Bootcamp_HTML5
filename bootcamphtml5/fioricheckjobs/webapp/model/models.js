7171652d74ef3a3b0877e34d7d544f8ac59de909
Vijay Singh Rajput <vijay.rajput@gmail.com>
2016-04-27 20:53:10 +0300

    Update models.js

--- bootcamphtml5/fioricheckjobs/webapp/model/models.js
+++ bootcamphtml5/fioricheckjobs/webapp/model/models.js
@@ -1,16 +1,26 @@
 sap.ui.define([
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/Device"
-], function(JSONModel, Device) {
-	"use strict";
+		"sap/ui/model/json/JSONModel",
+		"sap/ui/Device"
+	], function (JSONModel, Device) {
+		"use strict";
 
-	return {
+		return {
 
-		createDeviceModel: function() {
-			var oModel = new JSONModel(Device);
-			oModel.setDefaultBindingMode("OneWay");
-			return oModel;
-		}
+			createDeviceModel : function () {
+				var oModel = new JSONModel(Device);
+				oModel.setDefaultBindingMode("OneWay");
+				return oModel;
+			},
+
+			createFLPModel : function () {
+				var fnGetUser = jQuery.sap.getObject("sap.ushell.Container.getUser"),
+					bIsShareInJamActive = fnGetUser ? fnGetUser().isJamActive() : false,
+					oModel = new JSONModel({
+						isShareInJamActive: bIsShareInJamActive
+					});
+				oModel.setDefaultBindingMode("OneWay");
+				return oModel;
+			}
 		// ***************************************************************************************************
 		// Start of Custom code 
 		// User Information Model
@@ -19,7 +29,7 @@ sap.ui.define([
 
 		createUserModel: function() {
 		
-			var oModel = new JSONModel(window.oRootPath + "/destinations/BootCampDestination/JobEnrollmentDemo/GetUserAttributes");
+			var oModel = new JSONModel("/destinations/BootCampDestination/JobEnrollmentDemo/GetUserAttributes");
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
 		}
@@ -27,6 +37,7 @@ sap.ui.define([
 		// End of Custom code 
 		// ***************************************************************************************************
 
-	};
+		};
 
-});
+	}
+);

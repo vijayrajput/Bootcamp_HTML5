28dc4de42053f8a54bdb793ad38b338ba855a1ef
Vijay Singh Rajput <vijay.rajput@gmail.com>
2016-04-27 22:08:48 +0300

    Update formatter.js

--- bootcamphtml5/fioricheckapplications/webapp/model/formatter.js
+++ bootcamphtml5/fioricheckapplications/webapp/model/formatter.js
@@ -1,21 +1,22 @@
-sap.ui.define([], function() {
-	"use strict";
+sap.ui.define([
+	], function () {
+		"use strict";
 
-	return {
-		/**
-		 * Rounds the currency value to 2 digits
-		 *
-		 * @public
-		 * @param {string} sValue value to be formatted
-		 * @returns {string} formatted currency value with 2 digits
-		 */
-		currencyValue: function(sValue) {
-			if (!sValue || sValue===0) {
-				return ".";
+		return {
+			/**
+			 * Rounds the currency value to 2 digits
+			 *
+			 * @public
+			 * @param {string} sValue value to be formatted
+			 * @returns {string} formatted currency value with 2 digits
+			 */
+			currencyValue : function (sValue) {
+				if (!sValue) {
+					return "";
+				}
+
+				return parseFloat(sValue).toFixed(2);
 			}
-
-			return sValue;
-		},
 		// ***************************************************************************************************
 		// Start of Custom code 
 		// ***************************************************************************************************			
@@ -25,6 +26,7 @@ sap.ui.define([], function() {
 		 * @param {string} sValue the number string to be rounded
 		 * @returns {string} sValue with 2 digits rounded
 		 */
+		 ,
 		numberUnit: function(sValue) {
 			if (!sValue) {
 				return "0";
@@ -40,7 +42,8 @@ sap.ui.define([], function() {
 		}
 		// ***************************************************************************************************
 		// End of Custom code 
-		// ***************************************************************************************************				
-	};
+		// ***************************************************************************************************	
+		};
 
-});
+	}
+);

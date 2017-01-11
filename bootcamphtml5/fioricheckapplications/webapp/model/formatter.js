9b3a375a5e68d931534a51c70640c473cbf1fb2b
Vijay Singh Rajput <vijay.rajput@gmail.com>
2017-01-11 12:37:04 +0530

    Update formatter.js

--- bootcamphtml5/fioricheckapplications/webapp/model/formatter.js
+++ bootcamphtml5/fioricheckapplications/webapp/model/formatter.js
@@ -1,32 +1,31 @@
-sap.ui.define([
-	], function () {
-		"use strict";
+sap.ui.define([], function() {
+	"use strict";
 
-		return {
-			/**
-			 * Rounds the currency value to 2 digits
-			 *
-			 * @public
-			 * @param {string} sValue value to be formatted
-			 * @returns {string} formatted currency value with 2 digits
-			 */
-			currencyValue : function (sValue) {
+	return {
+		/**
+		 * Rounds the currency value to 2 digits
+		 *
+		 * @public
+		 * @param {string} sValue value to be formatted
+		 * @returns {string} formatted currency value with 2 digits
+		 */
+		currencyValue: function(sValue) {
 				if (!sValue) {
 					return "";
 				}
 
 				return parseFloat(sValue).toFixed(2);
 			}
-		// ***************************************************************************************************
-		// Start of Custom code 
-		// ***************************************************************************************************			
-		/**
-		 * Rounds the number unit value to 2 digits
-		 * @public
-		 * @param {string} sValue the number string to be rounded
-		 * @returns {string} sValue with 2 digits rounded
-		 */
-		 ,
+			// ***************************************************************************************************
+			// Start of Custom code 
+			// ***************************************************************************************************			
+			/**
+			 * Rounds the number unit value to 2 digits
+			 * @public
+			 * @param {string} sValue the number string to be rounded
+			 * @returns {string} sValue with 2 digits rounded
+			 */
+			,
 		numberUnit: function(sValue) {
 			if (!sValue) {
 				return "0";
@@ -34,16 +33,15 @@ sap.ui.define([
 			return sValue;
 		},
 		linkFormat: function(sValue) {
-			if (sValue) {
-				//return "https://www.ets.org/Media/Tests/TOEFL/pdf/SampleQuestions.pdf"+value;}
-				//var oRootPath = this.getView().getModel("device").getProperty("/rootPath");
-				return window.oRootPath + "/destinations/BootCampDestination/JobEnrollmentDemo/DocManagement?type=cv&empid=" + sValue;
+				if (sValue) {
+					//return "https://www.ets.org/Media/Tests/TOEFL/pdf/SampleQuestions.pdf"+value;}
+					//var oRootPath = this.getView().getModel("device").getProperty("/rootPath");
+					return  "/BootCampDestination/JobEnrollmentDemo/DocManagement?type=cv&empid=" + sValue;
+				}
 			}
-		}
-		// ***************************************************************************************************
-		// End of Custom code 
-		// ***************************************************************************************************	
-		};
+			// ***************************************************************************************************
+			// End of Custom code 
+			// ***************************************************************************************************	
+	};
 
-	}
-);
+});
